8444d3e7030d660bf88c91f9e7a1d98a7e07e788
Muharax <adriangajda@o2.pl>
2023-08-07 23:42:33 +0200

    +tablicaTolerancji

--- components/mainWindow.js
+++ components/mainWindow.js
@@ -7,6 +7,64 @@ function Pasowania() {
   const [inputValue, setInputValue] = useState("");
   const [showOtworu, setShowOtworu] = useState(false);
   const [showWalu, setShowWalu] = useState(false);
+  const start = 7;
+  const end = 14;
+  const itemsH = Array.from({length: end - start + 1}, (_, i) => `H${start + i}`);
+
+
+
+  let tablicaTolerancji = {
+    H6: [
+      {przedzial: [1,2,3], wartosc: [+6, 0]},
+      {przedzial: [4,5,6], wartosc: [+8, +0]},
+      {przedzial: [7,8,9,10], wartosc: [+9, +0]},
+    ],
+    H7: [
+      {przedzial: [1,2,3], wartosc: [+10, 0]},
+      {przedzial: [4,5,6], wartosc: [+12, +0]},
+      {przedzial: [7,8,9,10], wartosc: [+15, +0]},
+    ],
+    H8: [
+      {przedzial: [1,2,3], wartosc: [+14, 0]},
+      {przedzial: [4,5,6], wartosc: [+18, 0]},
+      {przedzial: [7,8,9,10], wartosc: [+22, +0]},
+    ],
+    H9: [
+      {przedzial: [1,2,3], wartosc: [+25, 0]},
+      {przedzial: [4,5,6], wartosc: [+30, 0]},
+      {przedzial: [7,8,9,10], wartosc: [+36, +0]},
+    ],
+    H10: [
+      {przedzial: [1,2,3], wartosc: [+30, 0]},
+      {przedzial: [4,5,6], wartosc: [+48, 0]},
+    ],
+    //...podobnie dla innych wartości H
+  };
+  
+
+
+  const znajdzWartosc = (input, selectedValue) => {
+    const tolerancje = tablicaTolerancji[selectedValue];
+    if (!tolerancje) {
+      return ["nieznana", "nieznana"]; // albo jakaś inna domyślna wartość
+    }
+  
+    const inputNum = parseInt(input);
+    for(let i = 0; i < tolerancje.length; i++) {
+      if (tolerancje[i].przedzial.includes(inputNum)) {
+        return tolerancje[i].wartosc;
+      }
+    }
+    return ["nieznana", "nieznana"]; // albo jakaś inna domyślna wartość
+  };
+  
+
+  const onInputChange = (value) => {
+    setInputValue(value);
+  };
+
+  const wartosc = znajdzWartosc(inputValue, selectedValue);
+
 
   return (
     <View>
@@ -33,29 +91,83 @@ function Pasowania() {
                 selectedValue={selectedValue}
                 onValueChange={(itemValue) => setSelectedValue(itemValue)}
               >
-                <Picker.Item label="h2" value="h1" />
-                <Picker.Item label="h2" value="h2" />
-                <Picker.Item label="h3" value="h3" />
-                <Picker.Item label="h4" value="h4" />
-                <Picker.Item label="h5" value="h5" />
-                <Picker.Item label="h6" value="h6" />
-                <Picker.Item label="h7" value="h7" />
-                <Picker.Item label="h8" value="h8" />
-                <Picker.Item label="h9" value="h9" />
-                <Picker.Item label="h10" value="h10" />
-                <Picker.Item label="h11" value="h11" />
-                <Picker.Item label="h12" value="h12" />
+                
+              {Object.keys(tablicaTolerancji).map((item) => (
+                <Picker.Item key={item} label={item} value={item} />
+              ))}
+
               </Picker>
             </View>
           </View>
 
-          <View style={styles.wartosc}>
-            <Text>
-              {`Wartość: ${inputValue}${selectedValue} tolerancja XXX`}
-            </Text>
+          <View style={styles.srodek}>
+
+            <View style={styles.naglowekOtworow}>
+              <Text style={styles.wartosciii}>
+                  {`Wartość: ${inputValue}${selectedValue} tolerancja `}
+              </Text>
+            </View>
+            
+            <View>
+              <Text style={styles.superscript}>
+                 {`${wartosc[0]} μm `}
+              </Text>
+              <Text style={styles.subscript}>
+                {`${wartosc[1]} μm `}
+              </Text>
+            </View>
+            
           </View>
 
         </View>}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -167,8 +279,16 @@ function MainWindow() {
 }
 
 const styles = StyleSheet.create({
-  wartosc: {
+  naglowekOtworow: {
+    top: -13,
+  },
+  wartosciii: {
     padding: 15,
+  },
+  srodek: {
+    flexDirection: 'row',
+    // justifyContent: 'center',
+    // alignItems: 'center',
   },
   wLinii: {
     flexDirection: 'row',
@@ -241,6 +361,16 @@ const styles = StyleSheet.create({
   tolerancjaWalu: {
 
 
+  },
+  superscript: {
+    fontSize: 10,
+    lineHeight: 10,
+    top: -10, // lub inna wartość, jeżeli potrzebujesz
+  },
+  subscript: {
+    fontSize: 10,
+    lineHeight: 20,
+    bottom: -6, // lub inna wartość, jeżeli potrzebujesz
   },
 });
 
